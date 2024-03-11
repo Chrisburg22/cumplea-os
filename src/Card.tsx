32a70875bf453c7af9459cf3d5a470150aef9271
Christian Ramos <94751604+Chrisburg22@users.noreply.github.com>
2024-03-11 09:50:46 -0600

    Create cumpleaños card

---
+++ src/Card.tsx
@@ -0,0 +1,33 @@
+export const Card = () => {
+  return (
+    <div className="birthdayCard">
+      <div className="cardFront">
+        <h3 className="happy">Feliz Cumpleaños Jonathan!</h3>
+        <div className="balloons">
+          <div className="balloonOne" />
+          <div className="balloonTwo" />
+          <div className="balloonThree" />
+          <div className="balloonFour" />
+        </div>
+      </div>
+      <div className="cardInside">
+        <h3 className="back">¡Feliz cumpleaños a uno de mis mejores amigos! 🎉 </h3>
+        <p>Querido Señor Jon</p>
+        <p>
+          Hoy es un día especial para celebrar la maravillosa persona que eres.
+          Que este nuevo año de vida te traiga abundancia de alegría, éxitos y
+          momentos inolvidables. Que cada día esté lleno de risas, amor y sueños
+          cumplidos. Gracias por ser esa luz que ilumina nuestra amistad. Tu
+          positividad y bondad hacen que la vida sea aún más hermosa. Que este
+          día esté lleno de sorpresas agradables y rodeado de las personas que
+          te quieren. Que este nuevo año te brinde oportunidades emocionantes y
+          experiencias inolvidables. Estoy agradecido por cada recuerdo
+          compartido y emocionado por los momentos que aún nos esperan.
+          ¡Brindemos por ti en tu día especial! ¡Que tengas un cumpleaños lleno
+          de amor, felicidad y todo lo maravilloso que la vida tiene para
+          ofrecer! 🥳🎂
+        </p>
+      </div>
+    </div>
+  );
+};
